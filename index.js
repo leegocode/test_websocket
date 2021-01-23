f1f176070ad1c0d24188d7e2c969434b893f50db
lee Helen <dzzzzzb@gmail.com>
2021-01-24 03:56:26 +0800

    server and client

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const SocketServer = require ('ws').Server;
 
 const server = express().listen(3000);
 const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+    console.log('server: a client is connected');
+    ws.on('close',() => console.log('server: a client disconnected'))
+    ws.on('message', (message) => {
+        console.log('server: receive message: es', message);
+        wss.clients.forEach(function each(client){
+            if(client !== ws && client.readyState === WebSocket.OPEN)
+            client.send(message)
+        })
+    })
+});
